bf829f315a937acd00ee16f8c48c7414d96fdf71
Penguen Umut <uuisik@gmail.com>
2021-12-30 00:44:25 +0300

    prod env

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-export const DATA_DIR: string = process.env.DATA_DIR || "./dist/data-dir/";
+export const DATA_DIR: string = process.env.DATA_DIR || "/datavolume1/";
 export const LISTEN_PORT: number = Number(process.env.PORT || 9900);
-export const ELECTRS_URL = process.env.DB_URL || "https://electrs.bitmatrix-aggregate.com/";
-export const DB_URL = process.env.DB_URL || "https://db.bitmatrix-aggregate.com/";
+export const ELECTRS_URL = process.env.DB_URL || "http://127.0.0.1:3000/";
+export const DB_URL = process.env.DB_URL || "http://127.0.0.1:8899/";
