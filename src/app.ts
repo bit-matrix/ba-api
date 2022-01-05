f332f0563a5d924a3ec4f30991cab36444e57177
Penguen Umut <uuisik@gmail.com>
2022-01-05 19:35:43 +0300

    updateChart for all pools

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { DATA_DIR, LISTEN_PORT, ELECTRS_URL } from "./env";
 import chartRoutes from "./routes/chartRoutes";
 import { init } from "@bitmatrix/esplora-api-client";
 import { updateChart } from "./business/updateChart";
+import { pools } from "./business/db-client";
 
 init(ELECTRS_URL);
 
@@ -38,7 +39,11 @@ server.listen(LISTEN_PORT, () => {
 
 const worker = async () => {
   try {
-    await updateChart("db7a0fa02b9649bb70d084f24412028a8b4157c91d07715a56870a161f041cb3", 10000);
+    const ps = await pools();
+    for (let i = 0; i < ps.length; i++) {
+      const p = ps[i];
+      await updateChart(p.id, 10000);
+    }
   } catch (error) {
     console.error(error);
   } finally {
