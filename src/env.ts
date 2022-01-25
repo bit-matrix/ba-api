5f542ebd7c56389f81254ba77cff1149f62a8892
Penguen Umut <uuisik@gmail.com>
2022-01-25 16:44:09 +0300

    new pool env

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-export const DATA_DIR: string = process.env.DATA_DIR || "./dist/data-dir/";
-export const LISTEN_PORT: number = Number(process.env.PORT || 9900);
-export const ELECTRS_URL = process.env.DB_URL || "https://electrs.bitmatrix-aggregate.com/";
-export const DB_URL = process.env.DB_URL || "https://db.bitmatrix-aggregate.com/";
+export const DATA_DIR: string = process.env.DATA_DIR || "/datavolume1/new-pool/";
+export const LISTEN_PORT: number = Number(process.env.PORT || 9950);
+export const ELECTRS_URL = process.env.DB_URL || "http://127.0.0.1:3000/";
+export const DB_URL = process.env.DB_URL || "http://127.0.0.1:4499/";
