53be810c9fc89ce489bf47846b7ff369e9444cb3
afarukcali <afarukcali@gmail.com>
2022-05-17 02:28:25 +0300

    local env updated.

--- src/env.local.ts
+++ src/env.local.ts
@@ -1,4 +1,4 @@
 export const DATA_DIR: string = process.env.DATA_DIR || "./dist/data-dir/";
 export const LISTEN_PORT: number = Number(process.env.PORT || 9901);
-export const ELECTRS_URL = process.env.DB_URL || "https://electrs.bitmatrix-aggregate.com/";
-export const DB_URL = process.env.DB_URL || "https://rocksdb.bitmatrix-aggregate.com/";
+export const ELECTRS_URL = process.env.DB_URL || "https://electrs.basebitmatrix.com/";
+export const DB_URL = process.env.DB_URL || "https://rocksdb.electrs.basebitmatrix.com/";
