1c42600be35c5cd2d7f1e0a8a0e3e8e40fbd355d
afarukcali <afarukcali@gmail.com>
2022-07-27 12:50:28 +0300

    unused endpoint removed.

--- src/business/db-client/index.ts
+++ src/business/db-client/index.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 import { DB_URL } from "../../env";
 import { BmPtx, Pool } from "@bitmatrix/models";
 
-export const clear = (): Promise<void> => axios.delete<void>(DB_URL + "clear").then((res) => res.data);
-
 export const pools = (): Promise<Pool[]> => axios.get<Pool[]>(DB_URL + "pools").then((res) => res.data);
 export const pool = (asset: string): Promise<Pool> => axios.get<Pool>(DB_URL + "pools/" + asset).then((res) => res.data);
 
