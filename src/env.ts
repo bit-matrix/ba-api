1eee5c6d5dd1e9eeb81cc9ff7db953fb19b74775
afarukcali <afarukcali@gmail.com>
2022-08-09 00:18:23 +0300

    env

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,3 @@
 export const DATA_DIR: string = process.env.DATA_DIR || "/datavolume1/";
 export const LISTEN_PORT: number = Number(process.env.PORT || 9902);
-export const ELECTRS_URL = process.env.ELECTRS_URL || "https://electrs.basebitmatrix.com/";
 export const DB_URL = process.env.DB_URL || "http://127.0.0.1:4450/";
