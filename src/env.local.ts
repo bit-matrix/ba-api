8bd5afbcc05e610c6691d6a192c10ee477b85b67
reyhanboydag <reyhanboydag@gmail.com>
2022-09-02 00:49:19 +0300

    unused files removed

--- src/env.local.ts
+++
@@ -1,3 +0,0 @@
-export const DATA_DIR: string = process.env.DATA_DIR || "./dist/data-dir/";
-export const LISTEN_PORT: number = Number(process.env.PORT || 9901);
-export const DB_URL = process.env.DB_URL || "https://rocksdb.electrs.basebitmatrix.com/";
--- src/env.prod.ts
+++
@@ -1,3 +0,0 @@
-export const DATA_DIR: string = process.env.DATA_DIR || "/datavolume1/new-pool/";
-export const LISTEN_PORT: number = Number(process.env.PORT || 9901);
-export const DB_URL = process.env.DB_URL || "http://127.0.0.1:4499/";
