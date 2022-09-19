75b26fe6d2224d743ca16a29b48cdac1fbfe4cfd
afarukcali <afarukcali@gmail.com>
2022-09-19 11:55:08 +0300

    unused configs removed.

--- src/env.ts
+++ src/env.ts
@@ -3,10 +3,6 @@ const base_host = process.env.BASE_HOST || "host.docker.internal";
 const api_internal_port = process.env.API_INTERNAL_PORT || "9901";
 const api_internal_data_dir = process.env.API_INTERNAL_DATA_DIR || "data_dir";
 
-const db_port = process.env.DB_PORT || "8001";
-const db_host = process.env.DB_HOST || base_host;
-const db_url = process.env.DB_URL || `http://${db_host}:${db_port}`;
-
 const redis_port = process.env.REDIS_PORT || "6379";
 const redis_host = process.env.REDIS_HOST || base_host;
 const redis_url = process.env.UI_REDIS_URL || `redis://${redis_host}:${redis_port}`;
@@ -14,8 +10,3 @@ const redis_url = process.env.UI_REDIS_URL || `redis://${redis_host}:${redis_por
 export const DATA_DIR: string = api_internal_data_dir + "/";
 export const LISTEN_PORT: number = Number(api_internal_port);
 export const REDIS_URL = redis_url;
-
-export const DB_URL = db_url + "/";
-
-// for production testing
-// export const DB_URL = process.env.DB_URL || "http://127.0.0.1:4499/";
